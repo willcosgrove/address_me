17190933ff99d22cf203da8ade93206897e5ac31
Will Cosgrove <will@willcosgrove.com>
2012-07-27 01:23:44 -0500

    Switched to geocoder, working dandily

--- address_me.gemspec
+++ address_me.gemspec
@@ -15,6 +15,7 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
   gem.version       = AddressMe::VERSION
 
+  gem.add_dependency("geocoder")
   gem.add_development_dependency("rspec")
   gem.add_development_dependency("guard-rspec")
 end
--- lib/address_me.rb
+++ lib/address_me.rb
@@ -1,15 +1,17 @@
+require 'geocoder'
 require "address_me/version"
 
 module AddressMe
   module_function
 
   def parse(string, options={})
+    result = Geocoder.search(string)[0]
     {
-      street: "Vista Ridge Dr N",
-      number: "7912",
-      zip_code: "76132",
-      city: "Fort Worth",
-      state: "TX"
+      street: result.address_components_of_type(:route)[0]["long_name"],
+      number: result.address_components_of_type(:street_number)[0]["long_name"],
+      zip_code: result.postal_code,
+      city: result.city,
+      state: result.address_components_of_type("administrative_area_level_1")[0]["short_name"]
     }
   end
 end
